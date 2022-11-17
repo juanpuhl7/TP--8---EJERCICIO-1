22c856a5b1ca94beabf901eed90c80eaefcf030d
johnmaster <juanpuhl404@gmail.com>
2022-11-16 22:12:23 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EJERCICIO_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int CANTDIAS=5;
+
+            string [] arreglos_Dias ={"Lunes","Martes","Miercoles","Jueves","Viernes"};
+
+            for(int i=0; i< CANTDIAS; i ++){
+                   Console.WriteLine(arreglos_Dias[i]);
+                    
+            }
+
+            
+        }
+    }
+}
